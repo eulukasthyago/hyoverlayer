30eec78a3d234119b3b91f4809f09fda19382879
Lucas Tiago <eulukasthyago@outlook.com>
2024-01-11 17:43:53 -0300

    :tada: initial commit

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Container from './layout/Container.svelte'
+
+  const ipc = window.electron.ipcRenderer
+
+  const handleOpenOverlay = (): void => {
+    ipc.invoke('overlay', true)
+  }
+  const handleCloseOverlay = (): void => {
+    ipc.invoke('overlay', false)
+  }
+</script>
+
+<Container>
+  <div class="p-2">
+    <div class="pt-4">
+      <button on:click={handleOpenOverlay}>Open Overlayers</button>
+      <br />
+      <button on:click={handleCloseOverlay}>Close Overlayers</button>
+    </div>
+  </div>
+</Container>
---
+++ src/renderer/src/layout/Container.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import '../css/global.css'
+</script>
+
+<div>
+  <div class="draggable flex justify-between p-2 bg-gray-900 text-gray-50">
+    <div>Nome do aplicativo</div>
+    <div class="no-draggable flex gap-2">
+      <button type="button">M</button>
+      <button type="button">m</button>
+      <button type="button">X</button>
+    </div>
+  </div>
+  <div class="bg-gray-50 w-full h-7 flex-row flex-wrap items-center px-2 hidden">
+    <ul>
+      <li>
+        <a href="#menu">Menu</a>
+        <ul>
+          <li>
+            <a href="#submenu">Submenu</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <slot />
+</div>
